77e362132d8d16550938399849e158699b5afcb9
Julio Collazos <amarullazo626@gmail.com>
2025-08-31 00:16:04 -0500

    Updated docs
    Helper function to keep an eye on the item columns

--- R/utils.R
+++ R/utils.R
@@ -5,3 +5,18 @@
 is_protected <- function(url) {
   startsWith(url, "https")
 }
+
+#' Item description columns
+#'
+#' @noRd
+#' @keywords internal
+item_columns <- function() {
+  c(
+    "id",
+    "title",
+    "description",
+    "picture_url",
+    "quantity",
+    "unit_price"
+  )
+}
